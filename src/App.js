fa4ea4ac09ea5462c068425d60c128c5b1c4d96b
Miguel Castillo <castilma95@gmail.com>
2022-07-27 18:33:30 -0600

    final

--- src/App.js
+++ src/App.js
@@ -8,38 +8,45 @@ function App() {
   const [colorInput, setColorInput] = useState("");
 
   const handleColorClick = (indx) => {
+    setCurrentColorIndex(indx)
   }
 
   const addNewColor = (e) => {
-
+    e.preventDefault();
+    setColors([...colors, colorInput])
   }
 
   const handleColorInputChange = (e) => {
-    // TODO
+    setColorInput(e.target.value)
   }
 
   return (
     <div className="App">
       <header>
-        <p>
-          Color: red
+        <p style={{color: colors[currentColorIndex]}}>
+          Color: {colors[currentColorIndex]}
         </p>
       </header>
       <main>
         <ul>
-            <li>
-              <button className='colored-btn'>
-                red
-              </button>
-            </li>
+            {colors.map((color, index) => {
+              return (
+                <li key={color}>
+                  <button  style={{backgroundColor: colors[index]}} className='colored-btn' onClick={() => handleColorClick(index)}>
+                    {colors[index]}
+                  </button>
+                </li>
+              )
+            })}
         </ul>
         <aside>
           <h2>Add a Color!</h2>
-          <form>
+          <form onSubmit={addNewColor}>
             <label htmlFor='color'>
               Color
             </label>
-            <input id="color" type="text" />
+            {/* <input id="color" type="text" value={colorInput} onChange={handleColorInputChange}/> */}
+            <input id="color" type="color" value={colorInput} onChange={handleColorInputChange}/>
             <input type="submit" value="add color"/>
           </form>
         </aside>
